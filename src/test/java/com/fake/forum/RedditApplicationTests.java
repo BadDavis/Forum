11c92a04b6ec647b7331ff692f6ef940edbd0af0
Dawid <davis693@o2.pl>
2021-04-18 11:39:36 +0200

    init commit

---
+++ src/test/java/com/fake/forum/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fake.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
